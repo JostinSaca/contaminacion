3a16a51eb9d31d4fb25d0b27d23a1d2b15ca746f
Jostin <jostinsaca53@gmail.com>
2026-06-23 22:35:42 -0500

    interfaz GUI realizada

---
+++ src/main/java/com/example/cli/Main.java
@@ -0,0 +1,10 @@
+package com.example.cli;
+
+import com.example.cli.SistemaCLI;
+
+public class Main {
+    public static void main(String[] args) {
+        SistemaCLI sistema = new SistemaCLI();
+        sistema.inicio();
+    }
+}
